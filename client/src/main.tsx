8a18dba996e9f2e8b313a7d3181c5afea5c5a045
hyuen <hyuen@lpstech.com>
2025-12-25 18:23:58 +0000

    code update

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,18 @@
+import React from "react";
 import { createRoot } from "react-dom/client";
+import { Router } from "wouter"; // make sure wouter is imported
 import App from "./App";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+const redirect = sessionStorage.redirect;
+delete sessionStorage.redirect;
+
+if (redirect && redirect !== location.pathname) {
+  history.replaceState(null, "", redirect);
+}
+
+createRoot(document.getElementById("root")!).render(
+  <Router base="/code_adventures_website">
+    <App />
+  </Router>
+);

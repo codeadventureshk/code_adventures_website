341a0cb0e163604286ed3c4d315a42cb0e93c00b
hyuen <hyuen@lpstech.com>
2025-12-25 18:38:12 +0000

    code update

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  root: 'client',  // <-- Add this
-  plugins: [react()],
+  root: 'client',       // <-- this tells Vite where index.html is
   build: {
-    outDir: '../dist', // output to root/dist
+    outDir: '../dist',  // <-- output goes to dist folder at project root
+    emptyOutDir: true
   },
+  plugins: [react()]
 })
